fd61ada826d0b59928def4fc424f38b4aaf067bc
stanleyig <oootehts@gmail.com>
2026-04-19 14:46:10 +0300

    add handlers

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import { PromptService } from './core/prompt/prompt.service';
+
+export class App {
+	async run() {
+		const res = await (new PromptService()).input<number>('Число', 'number');
+		console.log(res);
+		
+	}
+}
+
+const app = new App();
+app.run();
