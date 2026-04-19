d98d3af7e4f6451df0426519b7e4e1045260003e
stanleyig <oootehts@gmail.com>
2026-04-19 17:09:20 +0300

    project ready

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { DirExecuter } from "./commands/dir/dir.executor";
+import { FfmpegExecutor } from "./commands/ffmpeg/ffmpeg.executor";
+import { ConsoleLogger } from "./out/console-logger/console-logger";
+
+export class App {
+  async run() {
+    // new DirExecuter(ConsoleLogger.getInstance()).execute();
+    new FfmpegExecutor(ConsoleLogger.getInstance()).execute();
+  }
+}
+
+const app = new App();
+app.run();
